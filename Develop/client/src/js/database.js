b3963d80f607a0f54e01f57364878c87931e3766
Lauren Bentley <laurendebentley@gmail.com>
2023-03-16 14:13:03 -0400

    install.js logic

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,46 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  
+  console.log('POST to database')
+
+  //create a connection to the database and version we want to use
+  const grabDB = await openDB ('jate', 1);
+  //Create a new transaction and specify the database and data privelages ie. the database is 'jate' like we declared above, 'readwrite' is what we can do with 'jate'.
+  const dbTx = grabDB.transaction('jate', 'readwrite');
+  //Open the desired object store 
+  const store = dbTx.objectStore('jate');
+  //update text content in database
+  const request = store.put({ id: 1, value: content})
+
+  const result = await request;
+  console.log('Data saved', result)
+  return result
+
+  
+
+}
+
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+
+const grabDB = await openDB('jate', 1);
+
+const dbTx = grabDB.transaction('jate','readonly');
+
+const store = dbTx.objectStore('jate');
+
+const request = store.getAll();
+
+const result = await request;
+console.log('result.value', result);
+return result.value
+
+
+}
 
 initdb();
